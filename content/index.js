7e1a7b1d2efef284b5b42029ea498992b8b01194
Oleg Proskurin <regx@usul.su>
2019-09-24 19:52:52 +0300

    Fetch from CMS

--- content/index.js
+++ content/index.js
@@ -1,19 +1,50 @@
-const fetchData = () => new Promise(resolve => {
-  setTimeout(() => {
-    console.log('Content is fetched');
-    resolve();
-  }, 2000)
-})
+const { GraphQLClient } = require('graphql-request');
 
-const getContent = async () => {
-  await fetchData();
-  return {
-    name: 'Berlin Day',
-    title: 'From data',
-    description: 'Important content'
+const { development } = require('./config');
+
+const createClient = ({ endpoint, token }) => {
+  return new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+const client = createClient(development);
+
+const queryTexts = /* GraphQL */ `
+  query {
+    pieceOfTexts {
+      status
+      updatedAt
+      createdAt
+      id
+      markdown
+      key
+    }
   }
-}
+`;
+
+const fetchData = async() => {
+  const data = await client.request(queryTexts).then(res => res.pieceOfTexts);
+  const subContent = data.reduce(
+    (obj, item) => ({
+      ...obj,
+      [item.key]: item.markdown,
+    }),
+    {}
+  );
+  return {
+    welcome: subContent,
+  };
+};
+
+const getContent = async() => {
+  const content = await fetchData();
+  console.log('TCL: getContent -> content', content);
+  return content;
+};
 
 module.exports = {
-  getContent
+  getContent,
 };
