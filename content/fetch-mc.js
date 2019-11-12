da6084654490955d828dc6b1d9e9e10501efd217
Oleg Proskurin <regx@usul.su>
2019-11-12 18:20:13 +0300

    add markdown to mc's bio

--- content/fetch-mc.js
+++ content/fetch-mc.js
@@ -1,3 +1,5 @@
+const { markdownToHtml } = require('./markdown');
+
 const queryPages = /* GraphQL */ `
   query($conferenceTitle: ConferenceTitle, $eventYear: EventYear) {
     conf: conferenceBrand(where: { title: $conferenceTitle }) {
@@ -32,7 +34,7 @@ const fetchData = async(client, vars) => {
     .request(queryPages, vars)
     .then(res => res.conf.year[0].mcs);
 
-  const mcs = data.map(m => ({ ...m.speaker }));
+  const mcs = data.map(async m => ({ ...m.speaker, bio: await markdownToHtml(m.speaker.bio) }));
 
   return {
     mcs,
